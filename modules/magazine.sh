d16b841085d8dcdaa0fa35985121b89ac084ae3a
AS-Luar <rauassis2@gmail.com>
2025-07-18 17:32:49 -0300

    launch repo

---
+++ modules/magazine.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Bash configuration magazine
+
+# Source all module files
+source ~/.config/bash/modules/aliases.sh
+source ~/.config/bash/modules/paths.sh
+
+# Initialize zoxide
+eval "$(zoxide init bash)"
---
+++ modules/paths.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# PATH configurations
+
+export PATH="$HOME/.local/bin:$PATH"
+export PATH="$HOME/Development/software:$PATH"
+export PATH="$HOME/.local/share/ytfzf:$PATH"
+export PATH="$HOME/.config/bash/bin:$PATH"  # Add your bash scripts
+export PATH="$HOME/.config/bash/modules:$PATH"  # Add your bash scripts
+export TERM=xterm-256color
